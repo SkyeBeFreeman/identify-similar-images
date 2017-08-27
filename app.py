5a6b1c99b60876d2f940dc8d157ebdae906addfb
SkyeBeFreeman <928016560@qq.com>
2017-08-27 15:50:34 +0800

    画出两张图片的直方图

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
 def regularizePicture(img, size = (256, 256)):
     return img.resize(size).convert('RGB')
@@ -12,6 +13,13 @@ if __name__ == '__main__':
     # img1.show()
     # img2.show()
 
-    print(img1.histogram())
-    print(len(img1.histogram()))
-    # img2.histogram()
+    hg1 = img1.histogram()
+    # print(img1.histogram())
+    print('img1 样本点有', len(hg1))
+
+    hg2 = img2.histogram()
+    # print(img2.histogram())
+    print('img2 样本点有', len(hg2))
+
+    plt.plot(range(len(hg1)), hg1, range(len(hg2)), hg2)
+    plt.show()
