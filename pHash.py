a679e647c7854823b332a586a0945b20342d7f0d
SkyeBeFreeman <928016560@qq.com>
2017-09-13 11:21:02 +0800

    phash系数矩阵

--- pHash.py
+++ pHash.py
@@ -15,10 +15,36 @@ def getMatrix(img):
         matrix.append(pixel)
     return matrix
 
+# 计算系数矩阵
+def getCoefficient(length):
+    matrix = []
+    sqr = math.sqrt(1 / length)
+    value = []
+    for i in range(length):
+        value.append(sqr)
+    matrix.append(value)
+    for i in range(1, length):
+        value = []
+        for j in range(0, n):
+            value.append(math.sqrt(2.0 / length) * math.cos(i * math.pi * (j + 0.5) / length))
+        matrix.append(value)
+
 # 计算感知哈希算法相似度
 def calpHashSimilarity(img1, img2):
     img1 = regularizeImage(img1)
     img2 = regularizeImage(img2)
+
+    matrix1 = getMatrix(img1)
+    matrix2 = getMatrix(img2)
+
+    DCT1 = DCT(matrix1)
+    DCT2 = DCT(matrix2)
+    
+
     hc1 = getHashCode(img1)
     hc2 = getHashCode(img2)
     return compHashCode(hc1, hc2)
+
+if __name__ == '__main__':
+    matrix = [[1,2], [3, 4]]
+    print(len(matrix))
